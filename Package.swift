58ef781e0da964b26bf2a6fbc9dcd39bb1a0b3e7
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-17 19:15:41 +0100

    Add perfect dependencies

--- Package.swift
+++ Package.swift
@@ -9,5 +9,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "VabeExample"
+    name: "VabeExample",
+    targets: [
+        Target(
+            name: "VabePresentation",
+            dependencies: []
+        )
+    ],
+    dependencies: [
+        .Package(
+            url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+            versions: Version(0,0,0) ..< Version(10,0,0)
+        ),
+        .Package(
+            url: "https://github.com/PerfectlySoft/Perfect-MySQL.git",
+            versions: Version(0,0,0) ..< Version(10,0,0)
+        )
+    ],
+    exclude: []
 )
