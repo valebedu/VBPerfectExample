cbb3ac3be6c5dc3a17fdbb6bb34034b87ee0ef6f
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-17 17:58:31 +0100

    Generate swift package

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  VabeExample
+//
+//  Created by Valentin Bercot on 17/02/2017.
+//
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "VabeExample"
+)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,14 @@
+//
+//  LinuxMain.swift
+//  VabeExample
+//
+//  Created by Valentin Bercot on 17/02/2017.
+//
+//
+
+import XCTest
+@testable import VabeExampleTests
+
+XCTMain([
+     testCase(VabeExampleTests.allTests)
+])
